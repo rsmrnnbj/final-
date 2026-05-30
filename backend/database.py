33addeb111725537c7d6d89a9aef60d99b54fccb
rsmrnnbj <anneee266333@gmail.com>
2026-05-30 10:40:42 +0800

    Update database.py

--- backend/database.py
+++ backend/database.py
@@ -2,23 +2,44 @@
 import psycopg2
 from psycopg2.extras import RealDictCursor
 
-# 🔑 MAIN TRANSACTIONAL DATABASE CONFIG
 DB_CONFIG = {
     "dbname": "ecommerce_main_db",
-    "user": "myuser",
+    "user": "rose",
     "password": "anne2663",
-    "host": "168.231.118.191",
+    "host": "168.231.118.191", 
     "port": "5432"
 }
 
-# 🔑 NEW SEPARATE REPORTING DATABASE CONFIG
 REPORT_DB_CONFIG = {
     "dbname": "ecommerce_reporting_db", 
-    "user": "myuser",
+    "user": "rose",
     "password": "anne2663",
-    "host": "168.231.118.191",
+    "host": "168.231.118.191", 
     "port": "5432"
 }
+
+import os
+# from dotenv import load_dotenv
+# Load the .env file
+# load_dotenv()
+
+def get_db_connection():
+    return psycopg2.connect(
+        dbname=os.getenv("MAIN_DB_NAME"),
+        user=os.getenv("MAIN_DB_USER"),
+        password=os.getenv("MAIN_DB_PASS"),
+        host=os.getenv("MAIN_DB_HOST"), 
+        port="5432"
+    )
+
+def get_reporting_db_connection():
+    return psycopg2.connect(
+        dbname=os.getenv("REPORT_DB_NAME"),
+        user=os.getenv("REPORT_DB_USER"),
+        password=os.getenv("REPORT_DB_PASS"),
+        host=os.getenv("REPORT_DB_HOST"),
+        port="5432"
+    )
 
 def get_db_connection():
     """Helper function to open a clean connection to your main store database."""
